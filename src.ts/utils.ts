fa3e63caaecb6baf0ae37ac92dc9be3dfa853768
Richard Moore <github@ricmoo.com>
2023-03-20 14:58:29 -0400

    Added rootdir to utils.

--- src.ts/utils.ts
+++ src.ts/utils.ts
@@ -6,6 +6,8 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+export const rootdir = path.resolve(__dirname, "..");
+
 export function hexlify(data: Uint8Array): string {
     const result = [ "0x" ];
 
